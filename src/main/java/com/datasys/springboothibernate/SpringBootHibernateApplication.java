09202ec24d89b2549597a25717508edcb3fbdd44
xcodermonkey <sini.venkatreddy@gmail.com>
2018-08-25 22:39:03 +0530

    commit

--- src/main/java/com/datasys/springboothibernate/SpringBootHibernateApplication.java
+++ src/main/java/com/datasys/springboothibernate/SpringBootHibernateApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-@EnableAutoConfiguration(exclude=JpaRepositoriesAutoConfiguration.class)
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+
+@EnableAutoConfiguration(exclude={JpaRepositoriesAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
-
-
 public class SpringBootHibernateApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/datasys/springboothibernate/model/Person.java
+++ src/main/java/com/datasys/springboothibernate/model/Person.java
@@ -9,22 +9,45 @@ import javax.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @Entity
-@Getter
-@Setter
 public class Person {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	
+	
 	private String name;
 	
 	@JsonFormat(shape = JsonFormat.Shape.STRING,pattern="dd/mm/yyyy")
 	private Date dob;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	
+	
+	
 	
 	
 }
