755c463adece6b77d1fcae72333cba3736c2e2cc
herokuappsdevelopment <sini.venkatreddy@gmail.com>
2018-08-26 19:36:38 +0530

    adding lombak

--- src/main/java/com/datasys/springboothibernate/model/Person.java
+++ src/main/java/com/datasys/springboothibernate/model/Person.java
@@ -8,46 +8,27 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Data
+@Setter
+@Getter
 public class Person {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@JsonProperty
 	private int id;
 	
-	
+	@JsonProperty
 	private String name;
 	
 	@JsonFormat(shape = JsonFormat.Shape.STRING,pattern="dd/mm/yyyy")
 	private Date dob;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Date getDob() {
-		return dob;
-	}
-
-	public void setDob(Date dob) {
-		this.dob = dob;
-	}
-	
-	
-	
-	
 	
 }
